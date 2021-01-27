a9f052336974b984bd11cb44a44a1d41efd26dd0
tsengyiching <yictseng@student.42lyon.fr>
2021-01-27 16:54:46 +0100

    add minikube ip

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # Start minikube
 # --v=7 --alsologtostderr is for debugging mode
 # need to remove it later
-minikube start --vm-driver=hyperkit --v=7 --alsologtostderr
+minikube start --driver=hyperkit
 
 # Use the docker daemon from minikube
 eval $(minikube docker-env)
@@ -10,7 +10,12 @@ eval $(minikube docker-env)
 # Namespace is a virtual cluster supported by K8s
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+export	MinikubeIP=$(minikube ip)
+echo "      - $MinikubeIP-$MinikubeIP" >> srcs/metallb.yaml
 kubectl apply -f srcs/metalLB.yaml
 
 # Create the MetalLB scret memberlist
 kubectl create secret generic -n metallb-system memberlist  --from-literal=secretkey="$(openssl rand -base64 128)"
+
+docker build -t my_nginx srcs/nginx
+kubectl apply -f srcs/nginx.yaml
