d6d42e2a9affebb4795e69821dd1e58e9942a083
tsengyiching <yictseng@student.42lyon.fr>
2021-01-29 12:36:56 +0100

    update setup.sh, add mysql.yaml

--- setup.sh
+++ setup.sh
@@ -1,21 +1,30 @@
+#!/bin/sh
+
 # Start minikube
 # --v=7 --alsologtostderr is for debugging mode
-# need to remove it later
+# --driver=virtualbox for school mac
+# --driver=docker on vm
+minikube delete
 minikube start --driver=hyperkit
 
 # Use the docker daemon from minikube
 eval $(minikube docker-env)
 
+# Build images
+docker build -t my_nginx srcs/nginx
+docker build -t my_mysql srcs/mysql
+
 # Apply the MetalLB manifest yaml files, create controller and speaker
 # Namespace is a virtual cluster supported by K8s
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
-export	MinikubeIP=$(minikube ip)
-echo "      - $MinikubeIP-$MinikubeIP" >> srcs/metallb.yaml
-kubectl apply -f srcs/metalLB.yaml
-
 # Create the MetalLB scret memberlist
 kubectl create secret generic -n metallb-system memberlist  --from-literal=secretkey="$(openssl rand -base64 128)"
 
-docker build -t my_nginx srcs/nginx
+# Get minikube ip and apply it with metalLB 
+export	MinikubeIP=$(minikube ip)
+echo "      - $MinikubeIP-$MinikubeIP" >> srcs/metallb.yaml
+
+kubectl apply -f srcs/metalLB.yaml
 kubectl apply -f srcs/nginx.yaml
+kubectl apply -f srcs/mysql.yaml
