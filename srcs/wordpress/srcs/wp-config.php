9f245d05b36098deb918723611e5df1ef95c4c00
tsengyiching <yictseng@student.42lyon.fr>
2021-02-01 17:41:16 +0100

    add wp-conf.php for wordpress database, fix error data connection

---
+++ srcs/wordpress/srcs/wp-config.php
@@ -0,0 +1,23 @@
+<?php
+# Config Mariadb | database
+define('DB_NAME', '$WP_DB_NAME');
+define('DB_USER', '$WP_USER');
+define('DB_PASSWORD', '$PASSWORD');
+define('DB_HOST', '$MYSQL_IP');
+define('DB_CHARSET', 'utf8');
+define('FS_METHOD', 'direct');
+define('DB_COLLATE', '');
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
+$table_prefix = 'wp_';
+define('WP_DEBUG', false);
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+require_once(ABSPATH . 'wp-settings.php');
+?>
