5517808d1dadceef22773883812d5e934384a91a
fabien <fabien.buthodgarcon@gmail.com>
2021-02-08 10:21:51 +0100

    enleve l'ip

--- srcs/ftps/srcs/start_ftp.sh
+++ srcs/ftps/srcs/start_ftp.sh
@@ -6,7 +6,3 @@ echo "admin:admin" | chpasswd
 openssl req -new -x509 -days 365 -nodes -out /etc/ssl/private/vsftpd.cert.pem -keyout /etc/ssl/private/vsftpd.key.pem -subj "/C=FR/ST=Lyon/L=Lyon/O=42 School/OU=42student/CN=localhost"
 chown root:root /etc/ssl/private/vsftpd.cert.*
 chmod 600 /etc/ssl/private/vsftpd.cert.*
-vsftpd -opasv_min_port=21000 -opasv_max_port=21010 -opasv_address= /etc/vsftpd/vsftpd.conf
-vsftpd -opasv_min_port=21000 -opasv_max_port=21010 -opasv_address=172.17.0.3 /etc/vsftpd/vsftpd.conf
-vsftpd -opasv_min_port=21000 -opasv_max_port=21010 -opasv_address=172.17.0.3 /etc/vsftpd/vsftpd.conf
-vsftpd -opasv_min_port=21000 -opasv_max_port=21010 -opasv_address=172.17.0.4 /etc/vsftpd/vsftpd.conf
