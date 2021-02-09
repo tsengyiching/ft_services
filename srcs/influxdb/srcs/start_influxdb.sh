8c409b0dfd423dd45eb0e7d8a7b4aa10f555c9dd
tsengyiching <yictseng@student.42lyon.fr>
2021-02-09 20:56:35 +0100

    fix grafana dashboard bug, but need to put influxdb srcs corretly

--- srcs/influxdb/srcs/start_influxdb.sh
+++ srcs/influxdb/srcs/start_influxdb.sh
@@ -3,6 +3,6 @@
 adduser -D "root"
 echo "root:root1234" | chpasswd
 
-chown -R root:root /var/influxdb
+chown -R root:root /var/lib/influxdb
 (telegraf conf &) & 
-exec su-exec influxdb /var/influxdb/influxd
+exec su-exec influxdb /var/lib/influxdb/influxd
