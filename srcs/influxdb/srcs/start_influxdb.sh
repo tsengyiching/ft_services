158bcb5b96a913b4e879e3fc2af2b0e76b0dc063
tsengyiching <yictseng@student.42lyon.fr>
2021-02-09 11:59:14 +0100

    add telegraf

--- srcs/influxdb/srcs/start_influxdb.sh
+++ srcs/influxdb/srcs/start_influxdb.sh
@@ -4,10 +4,5 @@ adduser -D "root"
 echo "root:root1234" | chpasswd
 
 chown -R root:root /var/influxdb
+(telegraf conf &) & 
 exec su-exec influxdb /var/influxdb/influxd
-
-influx << EOF
-CREATE DATABASE $INFLUXDB_NAME;
-CREATE USER "$INFLUXDB_USER" WITH PASSWORD '$PASSWORD' WITH ALL PRIVILEGES;
-GRANT ALL ON $INFLUXDB_NAME TO $INFLUXDB_USER;
-EOF
