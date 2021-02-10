6e1f6d96217780ffed24113bccdd897bf4285853
tsengyiching <yictseng@student.42lyon.fr>
2021-02-10 12:07:54 +0100

    grafana connect to influxdb, but no data

--- srcs/influxdb/srcs/start_influxdb.sh
+++ srcs/influxdb/srcs/start_influxdb.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-adduser -D "root"
-echo "root:root1234" | chpasswd
-
-chown -R root:root /var/lib/influxdb
-(telegraf conf &) & 
-exec su-exec influxdb /var/lib/influxdb/influxd
+(telegraf conf &) & influxd
