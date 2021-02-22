a3c17d3433c0bfdb359cf359c5544345e281dea3
tsengyiching <yictseng@student.42lyon.fr>
2021-02-22 10:49:33 +0100

    switch back to sleep infinite

--- srcs/mysql/srcs/start_mysql.sh
+++ srcs/mysql/srcs/start_mysql.sh
@@ -6,7 +6,9 @@ mariadb-install-db -u root
 mysqld -u root & sleep 5
 # Create database
 mysql -u root --execute="CREATE DATABASE wordpress;"
+# Create Wordpress user "admin", password "admin"
+mysql -u root --execute="CREATE USER 'admin'@'%' IDENTIFIED BY 'admin'; GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION; USE wordpress; FLUSH PRIVILEGES;"
 # Create PhpMyAdmin user "user", password "user"
 mysql -u root --execute="CREATE USER 'user'@'%' IDENTIFIED BY 'user'; GRANT ALL PRIVILEGES ON *.* TO 'user'@'%' WITH GRANT OPTION; USE wordpress; FLUSH PRIVILEGES;"
 # Keep container running
-(telegraf conf &) & /usr/bin/mysqld --user=root --datadir=/var/lib/mysql
+(telegraf conf &) & sleep infinite
